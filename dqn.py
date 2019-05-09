3e22e81c3a9c11bdb4ec37f659b62b9853aaf136
seungeunrho <seung_eun07@naver.com>
2019-05-09 20:30:59 +0900

    remove categorical import

--- dqn.py
+++ dqn.py
@@ -7,7 +7,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.distributions import Categorical
 
 class ReplayBuffer():
     def __init__(self):
