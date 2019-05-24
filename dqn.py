3f4667061655e2f46f7bd726ed0142b499964254
seungeunrho <seung_eun07@naver.com>
2019-05-24 21:45:31 +0900

    refactoring

--- dqn.py
+++ dqn.py
@@ -31,14 +31,12 @@ class ReplayBuffer():
 class Qnet(nn.Module):
     def __init__(self):
         super(Qnet, self).__init__()
-        self.fc1 = nn.Linear(4, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 2)
+        self.fc1 = nn.Linear(4, 256)
+        self.fc2 = nn.Linear(256, 2)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
       
     def sample_action(self, obs, epsilon):
