fa8a05bb18fe1f9b8f6613581a5e3af6d0528781
Jungsub Lim <jsrimr@naver.com>
2020-12-19 12:13:28 +0900

    Update dqn.py
    Shouldn't target be detached? (Learning was fine without detach though..)

--- dqn.py
+++ dqn.py
@@ -68,7 +68,7 @@ def train(q, q_target, memory, optimizer):
         q_a = q_out.gather(1,a)
         max_q_prime = q_target(s_prime).max(1)[0].unsqueeze(1)
         target = r + gamma * max_q_prime * done_mask
-        loss = F.smooth_l1_loss(q_a, target)
+        loss = F.smooth_l1_loss(q_a, target.detach())
         
         optimizer.zero_grad()
         loss.backward()
